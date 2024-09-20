4e1b7480b14601476da0352985ac91af8ea7c623
Mohit Raj Rathor <mohitrajrathor@gmail.com>
2024-09-20 13:11:34 +0530

    rental object created

--- src/Main.java
+++ src/Main.java
@@ -68,6 +68,32 @@ class Customer {
 
 }
 
+class Rental {
+    private Car car;
+    private Customer customer;
+    private int days;
+
+    public Rental(Car car, Customer customer, int days) {
+        this.car = car;
+        this.customer = customer;
+        this.days = days;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public int getDays() {
+        return days;
+    }
+}
+
+
+
 
 public class Main {
     public static void main(String[] args) {
